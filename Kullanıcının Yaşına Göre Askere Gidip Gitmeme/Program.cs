f33c03b5a3f77297aa362c0560eef112a02683ff
Serap BALCI <81075168+reset-34@users.noreply.github.com>
2024-08-01 15:56:28 +0300

    Add project files.

---
+++ Kullanıcının Yaşına Göre Askere Gidip Gitmeme/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Kullanıcının_Yaşına_Göre_Askere_Gidip_Gitmeme
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int yas = 22;
+            
+            if (yas>=20)
+            {
+                Console.WriteLine("Askere Gidebilirsin");
+                }
+            else
+            {
+                Console.WriteLine("Askere Gidemezsi");
+                             }
+            Console.ReadLine();
+
+
+        }
+    }
+}
